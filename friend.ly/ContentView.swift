d0cc548ebda326913a22338f95db3bfac2207472
martinnicola89 <martinnicola89@gmail.com>
2021-09-24 16:18:02 -0400

    Initial Commit

---
+++ friend.ly/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  friend.ly
+//
+//  Created by Mac on 2021-09-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ friend.ly/friend_lyApp.swift
@@ -0,0 +1,17 @@
+//
+//  friend_lyApp.swift
+//  friend.ly
+//
+//  Created by Mac on 2021-09-24.
+//
+
+import SwiftUI
+
+@main
+struct friend_lyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
